f5fd760385650e1f1c8154e46117a9a1d1122a2d
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-13 20:44:00 +0530

    update

--- TelCo.ColorCoder/NewFeature.cs
+++ TelCo.ColorCoder/NewFeature.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +6,12 @@ using System.Threading.Tasks;
 using System.Globalization;
 namespace Telco.ColorCoder
 {
-    class NewFeature
+    class ColorMapManual
     {
         public static string getFormattedLine(int pairNumber, string color_pair)
         {
             CultureInfo provider = new CultureInfo("en-us");
-            string temp = pairNumber.ToString(provider) + " - " + color_pair;
-            temp += "\n";
+            string temp = pairNumber.ToString(provider) + " - " + color_pair + "\n"; 
             return temp;
         }
         public static string GetManual(int numberOfMajorColors, int numberOfMinorColors)
@@ -30,6 +29,22 @@ namespace Telco.ColorCoder
             }
             return allPairs;
         }
-       
+        public static string FakeGetManual(int numberOfMajorColors, int numberOfMinorColors)
+        {
+            int outerLoopCount = 0, innerLoopCount = 0, pairNumber = 1;
+            string allPairs = "";
+            for (outerLoopCount = 0; outerLoopCount < numberOfMajorColors; outerLoopCount++)
+            {
+                for (innerLoopCount = 0; innerLoopCount < numberOfMinorColors; innerLoopCount++)
+                {
+                    string color_pair = numberOfMajorColors.ToString();
+                    color_pair += numberOfMinorColors.ToString();
+                    allPairs += color_pair;
+                    pairNumber++;
+                }
+            }
+            return allPairs;
+        }
+
     }
 }
